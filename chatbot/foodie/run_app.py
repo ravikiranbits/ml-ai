e3c8f0004c10dc7c8ed4947b1b6323ae25866e2e
Ravi Kanneganti <ravikiran.bits@gmail.com>
2020-05-25 23:34:55 +0530

    Removed keys

--- chatbot/foodie/run_app.py
+++ chatbot/foodie/run_app.py
@@ -11,9 +11,9 @@ warnings.simplefilter('ignore', ruamel.yaml.error.UnsafeLoaderWarning)
 nlu_interpreter = RasaNLUInterpreter('./models/nlu/default/assignment_rest_nlu')
 agent = Agent.load('./models/dialogue', interpreter = nlu_interpreter)
 
-input_channel = SlackInput('xoxp-414578144033-415276250005-415001861763-514929468c15cf69f1e48b089a0b9abf', #app verification token
-							'xoxb-414578144033-415001862019-e1MOyUxnKiOjMj40au9PS7EN', # bot verification token
-							'OhEdoAfEv8yWW5qa5YR3DcOB', # slack verification token
+input_channel = SlackInput('', #app verification token
+							'', # bot verification token
+							'', # slack verification token
 							True)
 
 agent.handle_channel(HttpInputChannel(5004, '/', input_channel))
